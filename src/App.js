de6144d8c9ba9d0c02bdcad1c962580c5a8b39a3
kanakaraju.gandreddi <>
2024-01-23 16:24:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import Card from "react-bootstrap/Card";
+
+import Button from "react-bootstrap/Button";
+import { Container, Col, Row } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="header">Header</div>
+      <div className="sidenav"> SideNav</div>
+      <div className="content">Content</div>
+      <div className="aside">Aside</div>
+      <div className="footer">Footer</div>
     </div>
   );
 }
